1f22a39d7c2bab5ff6accda69f6b5c47d18314cc
praneethb7 <praneeth.24bcs10081@sst.scaler.com>
2025-04-21 13:02:36 +0530

    Minor Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -31,6 +31,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics/>
   </QueryClientProvider>
 );
 
